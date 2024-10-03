ddf823323ddcd882e2c355dad61e3aa383ce16ee
MarcelloPescatore <pescatoremarcello@gmail.com>
2024-10-03 14:56:02 +0200

    set up the workspace for the second assigment

--- second_assignments/script.js
+++ second_assignments/script.js
@@ -1,2 +1,9 @@
 // link test for html 
 console.log('test');
+
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// create 2 variables that generate a random number
+
+// create a condition that compare the random numbere generated and stamp the higher number 

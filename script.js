de035e28584e8bc36cdd3a80e3c64ef1791cab28
MarcelloPescatore <pescatoremarcello@gmail.com>
2024-10-03 14:24:19 +0200

    set up workspace for the first assignment

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ console.log('test');
 // Mail
 // Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 
+// creo una lista di email valide ad accedere
 
+// creo un prompt per richiedere l'email all'utente
+
+// creo una condizione che controlla la validità dell'email
 
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
